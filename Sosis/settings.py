0a46c5f0acb3a84bd4e5fd6c56573e291402e630
bvsp2109 <54323479+bvsp2109@users.noreply.github.com>
2019-10-18 12:43:22 +0530

    Update settings.py

--- Sosis/settings.py
+++ Sosis/settings.py
@@ -128,3 +128,11 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='djangotest25@gmail.com'
+EMAIL_HOST_PASSWORD='Test@123'
+EMAIL_PORT='587'
+EMAIL_USE_TLS=True
